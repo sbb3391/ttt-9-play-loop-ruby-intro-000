15255734c0d53592fc52044ef213c40eded10da1
Sam Bequette <sbb3391@gmail.com>
2020-04-05 20:08:17 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -16,8 +16,23 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def won?
-  WIN_COMBINATIONS.each do |i|
-    return true if i > 1
-  end
-end
+winners = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  winners.each do |win|
+ if !position_taken?(board,win)
+   puts win
+ else
+   false
+ end
+ end
+ end
